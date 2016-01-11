1e34b1eb310b7481126c69d6ca056db8b737a593
Adhyan <adhyan_srivastava95@yahoo.com>
2016-01-12 00:58:25 +0530

    Implemented Movies Contract

---
+++ app/src/main/java/io/github/ad_os/moviemania/model/MoviesContract.java
@@ -0,0 +1,41 @@
+package io.github.ad_os.moviemania.model;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by adhyan on 11/1/16.
+ */
+public class MoviesContract {
+    public static final String CONTENT_AUTHORITY = "io.github.ad_os.moviesmania";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final class MovieEntry implements BaseColumns {
+        //table name
+        public static final String TABLE_MOVIES = "movie";
+        //columns
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_THUMBNAIL = "thumbnail_string";
+        public static final String COLUMN_plot = "plot";
+        public static final String COLUMN_RATING = "rating";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        public static final String COLUMN_POSTER = "poster_string";
+        public static final String COLUMN_FAVORITE = "favorite";
+
+        //create content uri
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI
+                .buildUpon()
+                .appendPath(TABLE_MOVIES)
+                .build();
+        public static final String CONTENT_DIR_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + TABLE_MOVIES;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + TABLE_MOVIES;
+
+        public static Uri buildMovieUriWithId(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+}
